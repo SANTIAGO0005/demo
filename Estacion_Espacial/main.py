49172de98458dcc65b18a3b5b9f1cf2b800ffff5
Santiago Cifuentes <cifuentess938@gmail.com>
2022-07-14 05:56:38 -0500

    "actualizacion de languaje"

---
+++ Estacion_Espacial/main.py
@@ -0,0 +1,20 @@
+
+
+
+
+while True:
+    print('''\t**** MENU PRINCIPAL ****
+    1.CREAR NAVES
+    2.INVENTARIO
+    3.SALIR
+    ''')
+    opc = int(input('Digite una opcion: '))
+
+    if opc==1:
+        pass
+    elif opc==2:
+        pass
+    elif opc ==3:
+        exit()
+    else:
+        print('Opcion invalida, vuelva introducir una Opcion')
